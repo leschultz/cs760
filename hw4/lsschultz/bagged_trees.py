8571b02e2e7bb4db5bb7b5b940f1d81a0a3e0530
Lane Schultz <laneenriqueschultz@gmail.com>
2019-04-02 09:36:33 -0500

    Fist round

--- hw4/lsschultz/bagged_trees.py
+++ hw4/lsschultz/bagged_trees.py
@@ -4,40 +4,20 @@ import DecisionTree as dt
 
 import numpy as np
 
-import argparse
 import json
+import sys
 
-parserdescription = 'Bootstrap Aggregation (Bagging)'
+if __name__ == "__main__":
+    np.random.seed(0)
 
-parser = argparse.ArgumentParser(
-                                 description=parserdescription
-                                 )
-parser.add_argument(
-                    'ntrees',
-                    type=int,
-                    help='The number of trees'
-                    )
-parser.add_argument(
-                    'max_depth',
-                    type=int,
-                    help='The maximum depth of trees'
-                    )
-parser.add_argument(
-                    'train',
-                    type=str,
-                    help='Path to training set'
-                    )
-parser.add_argument(
-                    'test',
-                    type=str,
-                    help='Path to testing set'
-                    )
-
-args = parser.parse_args()
+argsntrees = int(sys.argv[1])
+argsmax_depth = int(sys.argv[2])
+argstrain = sys.argv[3]
+argstest = sys.argv[4]
 
 # Load the training and testing data
-train = json.load(open(args.train, 'r'))
-test = json.load(open(args.train, 'r'))
+train = json.load(open(argstrain, 'r'))
+test = json.load(open(argstest, 'r'))
 
 train_meta = train['metadata']['features']
 train_data = np.array(train['data'])
@@ -51,9 +31,63 @@ y_train = train_data[:, -1]
 X_test = test_data[:, :-1]
 y_test = test_data[:, -1]
 
-# Train model
-predictor = dt.DecisionTree()
-predictor.fit(X_train, y_train, train_meta, max_depth=args.max_depth)
+indices = []
+probs = []
+preds = []
+for tree in range(argsntrees):
 
-# Predict
-y_pred = predictor.predict(X_test, prob=True)
+    choice = np.random.choice(
+                              X_train.shape[0],
+                              X_train.shape[0],
+                              )
+
+    X_train_strap = X_train[choice, :]
+    y_train_strap = y_train[choice]
+
+    # Train model
+    predictor = dt.DecisionTree()
+    predictor.fit(
+                  X_train_strap,
+                  y_train_strap,
+                  train_meta,
+                  max_depth=argsmax_depth
+                  )
+
+    # Predict
+    y_prob = predictor.predict(X_test, prob=True)
+    y_pred = predictor.predict(X_test)
+
+    indices.append(choice)
+    preds.append(y_pred)
+    probs.append(y_prob)
+
+indices = np.column_stack(indices)
+probs = np.array(probs)
+
+combined = np.apply_along_axis(np.argmax, 1, np.sum(np.array(probs), axis=0))
+predict = []
+for i in combined:
+    prediction = test_meta[-1][1][i]
+    predict.append(prediction)
+
+accuracy = len(y_test[predict == y_test])/len(y_test)
+
+for tree in indices:
+    out = ','.join(map(str, tree))
+    print(out, file=sys.stdout)
+
+print(file=sys.stdout)
+
+preds.append(predict)
+preds.append(y_test)
+preds = np.column_stack(preds)
+for tree in preds:
+    out = ','.join(map(str, tree))
+    print(out, file=sys.stdout)
+
+print(file=sys.stdout)
+
+print(
+      '{}'.format(np.round(accuracy, 16)),
+      file=sys.stdout
+      )
